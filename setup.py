2b451c3ff1b92dc27aa1a2e245f5eb8efd957197
Ashish Garg <ashish.garg@linux.com>
2022-01-31 18:46:44 +0530

    Add setup.py and MANIFEST.in for package building

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Python package that captures exceptions and notify them'
+LONG_DESCRIPTION = 'Python package that captures exceptions and notify them'
+
+setup(
+    name="capture", 
+    version=VERSION,
+    author="Ashish Garg",
+    author_email="ashish.garg@linux.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={
+        "jinja2": [],
+        "requests": [],
+    },
+    keywords=['python', 'exceptions', 'errors'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: Apache-2.0 License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
